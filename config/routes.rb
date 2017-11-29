a378e18c43bd46564ff560aec05c9c7ea5842237
yspoo <pooyising@gmail.com>
2017-11-29 10:51:39 +0800

    Removed admin.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
