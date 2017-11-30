8bca279a91434ecd211cfc5b9796708daa8fe940
yspoo <pooyising@gmail.com>
2017-11-30 11:56:54 +0800

    RSpec factory done.

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :note do
     title "MyString"
     body "MyText"
+    association :user
+
+    trait :invalid do
+      title nil
+    end
+
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence :username { |n| "username#{n}" }
+    sequence :email { |n| "user#{n}@example.com" }
+    password "password123"
   end
 end
