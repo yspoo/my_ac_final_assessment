4f873a19136b1165b2b041ba6030a0418559cdce
yspoo <pooyising@gmail.com>
2017-12-01 13:36:30 +0800

    Relationship model spec done.

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
 
+  # Associations
   it { is_expected.to belong_to(:user) }
   it { is_expected.to belong_to(:note) }
 
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
 
+  # Associations
+  it { is_expected.to belong_to(:follower).class_name("User") }
+  it { is_expected.to belong_to(:followed).class_name("User") }
+
+  # Validations
+  it { is_expected.to validate_presence_of(:follower_id) }
+  it { is_expected.to validate_presence_of(:followed_id) }
+
 end
