2a1cc5f197c87a4b5ecd1653b7099e1d72fd889d
yspoo <pooyising@gmail.com>
2017-12-01 13:27:05 +0800

    Like and Note MODEL specs done.

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
 
+  # Associations
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:likes).dependent(:destroy) }
+  it { is_expected.to have_many(:likers).class_name("User").through(:likes).source(:user) }
+
+  # Validations
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:user_id) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it { is_expected.to have_many(:notes).dependent(:destroy) }
-  it { is_expected.to belong_to(:user) }
 
 end
